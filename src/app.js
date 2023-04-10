83266a0d33396edc7d1f9b49106794a34c8aab4d
lucasmalacrida <lucasmd99@hotmail.com>
2023-04-10 17:22:02 -0300

    Implementada a Validação de Dados

--- src/app.js
+++ src/app.js
@@ -11,6 +11,10 @@ const tweets = [];
 app.post('/sign-up', (req, res) => {
     const { username, avatar } = req.body;
 
+    if ((!username) || (!avatar) || (typeof username !== 'string') || (typeof avatar !== 'string')) {
+        return res.status(400).send("Todos os campos são obrigatórios!");
+    }
+
     const newUser = { username, avatar };
     users.unshift(newUser);
     res.send("OK");
@@ -19,18 +23,23 @@ app.post('/sign-up', (req, res) => {
 app.post('/tweets', (req, res) => {
     const { username, tweet } = req.body;
     const user = users.find(x => x.username === username);
+
+    if ((!username) || (!tweet) || (typeof username !== 'string') || (typeof tweet !== 'string')) {
+        return res.status(400).send("Todos os campos são obrigatórios!");
+    }
+
     if (!user) {
         return res.send("UNAUTHORIZED");
     }
 
     const newTweet = { username, tweet }
-    newTweet.avatar = users.find( x => x.username === username).avatar;
+    newTweet.avatar = users.find(x => x.username === username).avatar;
     tweets.unshift(newTweet);
     res.send("OK");
 });
 
 app.get('/tweets', (req, res) => {
-    const lastTweets = tweets.slice(0,10);
+    const lastTweets = tweets.slice(0, 10);
     res.send(lastTweets);
 });
 
